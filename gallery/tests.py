f78699d9ef56c9f7d392478fc390bf7dae82a319
deisy <dt.macias@uniandes.edu.co>
2019-03-23 23:07:19 -0500

    Paso 4. Test para ver portafolios publicos por persona (ROJO)

--- gallery/tests.py
+++ gallery/tests.py
@@ -28,3 +28,16 @@ class GalleryTestCase(TestCase):
         response=self.client.post('/gallery/addUser/',json.dumps({"username": "testUser", "first_name": "Test", "last_name": "User", "password": "AnyPas#5", "email": "test@test.com"}), content_type='application/json')
         current_data=json.loads(response.content)
         self.assertEqual(current_data[0]['fields']['username'],'testUser')
+
+    def test_ver_imagen_persona(self):
+        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test',
+                                              last_name='test', email='test@test.com')
+        Image.objects.create(name='nuevo', url='No', description='testImage', type='jpg', user=user_model,
+                             is_public=True)
+        Image.objects.create(name='nuevo2', url='No', description='testImage', type='jpg', user=user_model,
+                             is_public=False)
+        url = '/gallery/' + str(user_model.id)
+        response = self.client.get(url, format='json')
+        current_data = json.loads(response.content)
+        self.assertEqual(len(current_data),1)
+        self.assertEqual(current_data[0]['fields']['name'],'nuevo')
