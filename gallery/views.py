c91542a423d33d04adae0e12efc186a0bc1f69cc
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2019-03-23 22:06:29 -0500

    Paso 1 servicio rest implementado (verde)

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+from .models import Image
+import json
 
 # Create your views here.
+@csrf_exempt
+def index(request):
+    images_list = []
+    return HttpResponse(serializers.serialize("json", images_list))
