c908ddecc5815eaaba2f4293f8bdabd3e62ef58b
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2019-03-24 18:08:31 -0500

    Paso 5. Refactor del código (azul)

--- gallery/tests.py
+++ gallery/tests.py
@@ -7,6 +7,11 @@ import json
 
 # Create your tests here.
 class GalleryTestCase(TestCase):
+    def setUp(self):
+        user_model = User.objects.create_user(username='test', password='1234', first_name='test',
+                                              last_name='test', email='test@test.com')
+        Image.objects.create(name='nuevo', url='No', description='testImage', type='jpg', user=user_model, is_public=True)
+        Image.objects.create(name='nuevo2', url='No', description='testImage', type='jpg', user=user_model, is_public=False)
 
     def test_list_images_status(self):
         url = '/gallery/'
@@ -14,14 +19,8 @@ class GalleryTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_count_images_list(self):
-        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test',
-                                              last_name='test', email='test@test.com')
-        Image.objects.create(name='nuevo', url='No', description='testImage', type='jpg', user=user_model, is_public=True)
-        Image.objects.create(name='nuevo2', url='No', description='testImage', type='jpg', user=user_model, is_public=False)
-
         response = self.client.get('/gallery/')
         current_data = json.loads(response.content)
-
         self.assertEqual(len(current_data), 2)
 
     def test_add_user(self):
@@ -30,12 +29,10 @@ class GalleryTestCase(TestCase):
         self.assertEqual(current_data[0]['fields']['username'],'testUser')
 
     def test_ver_imagen_persona(self):
-        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test',
+        user_model = User.objects.create_user(username='test2', password='1234', first_name='test',
                                               last_name='test', email='test@test.com')
-        Image.objects.create(name='nuevo', url='No', description='testImage', type='jpg', user=user_model,
-                             is_public=True)
-        Image.objects.create(name='nuevo2', url='No', description='testImage', type='jpg', user=user_model,
-                             is_public=False)
+        Image.objects.create(name='nuevo', url='No', description='testImage', type='jpg', user=user_model, is_public=True)
+        Image.objects.create(name='nuevo2', url='No', description='testImage', type='jpg', user=user_model, is_public=False)
         url = '/gallery/' + str(user_model.id) + '/'
         response = self.client.get(url, format='json')
         current_data = json.loads(response.content)
@@ -43,10 +40,10 @@ class GalleryTestCase(TestCase):
         self.assertEqual(current_data[0]['fields']['name'],'nuevo')
 
     def test_iniciar_Sesion(self):
-        user_model = User.objects.create_user(username='test', password='1234', first_name='test',
+        user_model = User.objects.create_user(username='test3', password='1234', first_name='test',
                                               last_name='test', email='test@test.com')
         response = self.client.post('/gallery/login/', json.dumps(
             {"username": user_model.username,"password": user_model.password}), content_type='application/json')
         current_data = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(current_data[0]['fields']['username'],'test')
+        self.assertEqual(current_data[0]['fields']['username'],'test3')
